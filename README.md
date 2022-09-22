c6108ced95983b5de64f2b5a3ae593ee808eeba0
azamatcoding <azamat.coding@gmail.com>
2022-09-23 02:39:39 +0500

    Store

--- README.md
+++ README.md
@@ -89,3 +89,13 @@ Specify how the app’s state changes in response to actions sent to the store
 Function that accepts state and action as arguments, and returns the next state of the application
 
 (previousState, action) => newState
+
+# Redux Store
+One store for the entire application
+
+Responsibilities
+* Holds application state
+* Allows access to state via **getState()**
+* Allows state to be updated via **dispatch(action)**
+* Registers listeners via **subscribe(listener)**
+* Handles unregistering of listeners via the function returned by **subscribe(listener)**
--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+const redux = require('redux')
+const createStore = redux.createStore
+
 const CAKE_ORDERED = 'CAKE_ORDERED'
 
 function orderCake() {
@@ -25,3 +28,15 @@ const reducer = (state = initialState, action) => {
       return state
   }
 }
+
+const store = createStore(reducer)
+console.log('initial State ', store.getState())
+
+// store.subscribe(() => console.log('update state ', store.getState()))
+const unsubscribe = store.subscribe(() => console.log('update state ', store.getState()))
+
+store.dispatch(orderCake())
+store.dispatch(orderCake())
+store.dispatch(orderCake())
+
+unsubscribe()
