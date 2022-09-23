7fe7afa4c14ee6d64f5bfa03d830278fa7cf55d4
azamatcoding <azamat.coding@gmail.com>
2022-09-23 12:00:13 +0500

    Bind Action Creators

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const redux = require('redux')
 const createStore = redux.createStore
+const bindActionCreators = redux.bindActionCreators
 
 const CAKE_ORDERED = 'CAKE_ORDERED'
 const CAKES_RESTOCKED = 'CAKES_RESTOCKED'
@@ -50,9 +51,15 @@ console.log('initial State ', store.getState())
 // store.subscribe(() => console.log('update state ', store.getState()))
 const unsubscribe = store.subscribe(() => console.log('update state ', store.getState()))
 
-store.dispatch(orderCake())
-store.dispatch(orderCake())
-store.dispatch(orderCake())
-store.dispatch(restockCake(3))
+// store.dispatch(orderCake())
+// store.dispatch(orderCake())
+// store.dispatch(orderCake())
+// store.dispatch(restockCake(3))
+
+const actions = bindActionCreators({orderCake, restockCake}, store.dispatch)
+actions.orderCake()
+actions.orderCake()
+actions.orderCake()
+actions.restockCake(3)
 
 unsubscribe()
